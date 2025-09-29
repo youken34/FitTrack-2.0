577f218cd50ba5b075bdfb781b53b499dc4224bc
Côme Bonal <come.bonal@gmail.com>
2025-09-29 16:24:20 +0200

    backend initialization

---
+++ frontend/src/app/services/user.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  private apiUrl = 'http://localhost:3000/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  updateUser(id: string, data: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, data);
+  }
+}
